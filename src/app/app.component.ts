6826eea2ab02bbbeda2079a2a16af557627831ea
Daniel Danielecki <daniel.danielecki@mail.com>
2018-09-25 15:58:37 +0300

    Edit: auto scroll to top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,18 @@ import {fadeAnimation} from './shared/fade.animation';
   animations: [fadeAnimation]
 })
 export class AppComponent {
-  title = 'app';
-
-  // TODO: Add animation.
   getRouterOutletState(routerOutlet: RouterOutlet) {
     const routeData = routerOutlet.activatedRouteData['animation'];
     return routeData ? routeData : 'rootPage';
+  }
+  onActivate(event) {
+    const scrollToTop = window.setInterval(() => {
+      const pos = window.pageYOffset;
+      if (pos > 0) {
+        window.scrollTo(0, pos - 10); // how far to scroll on each step
+      } else {
+        window.clearInterval(scrollToTop);
+      }
+    }, 16);
   }
 }
